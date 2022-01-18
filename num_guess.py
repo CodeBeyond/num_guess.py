3e6a75f2bac0aa3cda6b7dfe659da2d06af523cb
CodeBeyond <dancewaterdance@gmail.com>
2022-01-18 15:26:57 -0800

    s

--- num_guess.py
+++ num_guess.py
@@ -6,9 +6,9 @@ while numbers_guessed < 99999999999999999999999999999999999999999999999999999999
     guess = int(input())
     numbers_guessed = numbers_guessed + 1
     if guess < num:
-        print("Too low - try again.")
+        print("Too low - try again")
     elif guess > num:
-        print("Too high - try again.")
+        print("Too high - try again")
     else:
         if guess == num:
             print("You guessed in ", numbers_guessed, "tries.")
